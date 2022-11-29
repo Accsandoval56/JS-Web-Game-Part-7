babcdd93d9e4d01d322cf1ce242c932650d04acb
Ricardo Sandoval <accsandoval56@gmail.com>
2022-11-28 22:02:53 -0800

    First Commit

--- index.js
+++ index.js
@@ -5,14 +5,23 @@ const pc = newPlayableCharacter(100, 110)
 const npc = newNonPlayableCharacter(50, 300)
 
 // have the NPC start walking east immediately
-npc.walkEast()
 
-// Create the inventory
-const inventory = newInventory()
+const moveNPC = async () => { 
+    await npc.walkNorth(1400)
+    await npc.walkEast(1200)
+    await npc.walkSouth(300)
+    await npc.walkEast(1500)
+    await npc.walkSouth(1500)
+    await npc.walkWest(2700)
+    await npc.walkNorth(400)
+ }
+ moveNPC()
+
+const inventory = newInventory() // Create the inventory
 move(inventory).to(0, 0)
 
-// Create everything else
-move(newImage('assets/tree.png')).to(200, 450)
+
+move(newImage('assets/tree.png')).to(200, 450) // Create everything else
 move(newImage('assets/pillar.png')).to(350, 250)
 move(newImage('assets/pine-tree.png')).to(450, 350)
 move(newImage('assets/crate.png')).to(150, 350)
